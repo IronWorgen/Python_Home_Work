01668a16f662e452214a6d6e9a7410e62a7b01ed
ivasenko <ivasenko98@list.ru>
2023-05-30 13:43:18 +0300

    sem 8 task1 update

--- HW_sem8/data.py
+++ HW_sem8/data.py
@@ -1,9 +1,22 @@
 import codecs
 
+def get_list_number_plus_name():
+    contacts = ""
+    with codecs.open("HW_sem8\телефоны.txt", "r", "utf-8") as data:
+        for i in data:
+            contacts+=i
+
+    contacts = contacts.split("\n")
+    contacts.pop(len(contacts)-1)
+    contacts = list(map(lambda x : x.split(" - "), contacts))
+    return contacts
+    
+
 def add_number(number , name):
     with codecs.open("HW_sem8\телефоны.txt", "a", "utf-8") as data:
         data.write(str(number)+" - "+ name+"\n")
-    data.close 
+    data.close
+     
 
 
 def print_numbers():
@@ -13,22 +26,16 @@ def print_numbers():
                 contacts+=i
             print(contacts)
         data.close 
+        
 
 
-def sort(name):
-    contacts = ""
-    with codecs.open("HW_sem8\телефоны.txt", "r", "utf-8") as data:
-        for i in data:
-            contacts+=i
-
-    contacts = contacts.split("\n")
-    contacts.pop(len(contacts)-1)
-    contacts = list(map(lambda x : x.split(" - "), contacts))
+def sorted(name):
+    contacts = get_list_number_plus_name()
     contacts = list(filter ( lambda x: x[1]==name, contacts))
 
     if (len(contacts)==0):
         print (f"Фамилия {name} не найдена\n")
-        return
+        return False
     
     result = ""
     for i in range(len(contacts)):
@@ -39,4 +46,51 @@ def sort(name):
     result+=contacts[len(contacts)-1]   
 
     print (result)
+    return True
+
+
+
+def replace_name(name, new_name):
+    with codecs.open("HW_sem8\телефоны.txt", "r", "utf-8") as data:
+        text = data.read()
+        text = text.replace( name, new_name)
+    data.close
+
+    with codecs.open("HW_sem8\телефоны.txt", "w", "utf-8") as data:
+        data.write(text)
+
+
+def delete_by_number(number):
+    contacts = get_list_number_plus_name()
+    new_contacts = list(filter (lambda x : x[0]!=number, contacts))
+
+    if (len(contacts)==len(new_contacts)):
+        print ("номер не найден")
+        return
+    
+    result = ""
+    for i in range(len(new_contacts)):
+        result +=new_contacts[i][0]+" - "+ new_contacts[i][1]+"\n"
+    with codecs.open("HW_sem8\телефоны.txt", "w", "utf-8") as data:
+        data.write(result)
+    data.close    
+    print (f"номер {number} удален")
+    
+
+def delete_by_name(name):
+    contacts = get_list_number_plus_name()
+    new_contacts = list(filter (lambda x : x[1]!=name, contacts))
+    if (len(contacts)==len(new_contacts)):
+        print ("Пользователь не найден")
+        return
+    
+    result = ""
+    for i in range(len(new_contacts)):
+        result +=new_contacts[i][0]+" - "+ new_contacts[i][1]+"\n"
+    with codecs.open("HW_sem8\телефоны.txt", "w", "utf-8") as data:
+        data.write(result)
+    data.close    
+    print (f"Контакт {name} удален")
+    
+
 
--- HW_sem8/task1.py
+++ HW_sem8/task1.py
@@ -4,51 +4,85 @@ import data
 def add_phone():
     phone_number = int(input("Телефон: "))
     name = input("Имя: ")
+    data.add_number(phone_number, name)
+    print()
 
-    data.add_number(phone_number , name)
-    
 
-def print_all():    
+def print_all():
     print("\nВаши контакты")
     data.print_numbers()
-    
-    
 
-def  search_by_last_name():
-    
+
+def search_by_last_name():
+
     print("\nВведите имя для поиска")
     name = input("Поиск: ")
-    data.sort(name)
+    if (data.sorted(name)):
+        print()
+
+        triger =  True
+        while triger:
+            new_name=name 
+            print(f"Введите:\n1 - чтобы изменить имя {name}\nq - вернуться")
+
+            user_input = input() 
+            if user_input=="1":
+                new_name = input(f"{name} -> ")
+                data.replace_name(name, new_name)
+                print("Имя успешно изменено\n")
+                name = new_name
+
+                triger = False
+            elif user_input=="q":
+                print()
+                return
+            else :
+                print ("Выберите вариант из списка")
+    print()            
 
 
-operation_list = {"1": {"добавить контакт": add_phone}, "2": {"вывести список контактов": print_all} , "3" : {"поиск по фамилии": search_by_last_name}}
+        
+
+
+def delete_number():
+    user_input = input("Введите номер: ")
+    data.delete_by_number(user_input)
+     
+    
+
+def delete_name():
+    print("Введите фамилию")
+
+    print("Удалить все номера по фамилии")      
+
+
+operation_list = {"1": {"добавить контакт": add_phone}, "2": {
+    "вывести список контактов": print_all}, "3": {"поиск по фамилии": search_by_last_name}}
+
+operation_list["4"]={"Удалить номер": delete_number}
+operation_list["5"]={"Удаление по фамилии ": delete_name}
 
 
 
-def run ():
-    print ("Привет")
+def run():
+    print("Привет")
     triger = True
     while triger:
-        
+
         for i in operation_list.keys():
             operation_name = list(operation_list.get(i).keys())[0]
             print(f"{i} - {operation_name}")
-        print ("q - завершить работу") 
+        print("q - завершить работу")
 
         user_input = input()
 
-        if user_input=='q':
+        if user_input == 'q':
             triger = False
         elif user_input in list(operation_list.keys()):
 
             list(operation_list.get(user_input).values())[0]()
         else:
-            print ("Выберите вариант из списка")    
-
-        
-    
-run ()
-    
+            print("Выберите вариант из списка")
 
 
-    
+run()
